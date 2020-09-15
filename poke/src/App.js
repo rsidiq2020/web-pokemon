3d1c8f2ec144e99a3f64f84020ba3e04a6fba8ad
sidiq95 <sidiq95@yaho.com>
2020-09-15 17:55:11 +0700

    firstpage pokemon

---
+++ poke/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import HeadApp from '../src/component/header.component';
+import FootApp from '../src/component/footer.component';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <HeadApp/>
+        <h2>Ini Isi Halaman Utama</h2>
+        <FootApp/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ poke/src/component/footer.component.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class FootApp extends Component {
+    render() {
+      return (
+        <div className="App">
+          <p className="App-footer">
+            Created by <i>https://sidiq.net</i> @2020.
+          </p>
+        </div>
+      );
+    }
+  }
+  
+  export default FootApp;
