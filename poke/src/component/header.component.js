d3fedfb49d69ebb6263f685b66e62081b8dd7c62
sidiq95 <sidiq95@yaho.com>
2020-11-17 10:29:43 +0700

    change css

--- poke/src/component/header.component.js
+++ poke/src/component/header.component.js
@@ -4,16 +4,14 @@ class HeadApp extends Component {
   render() {
     return (
       <div className="App-header">
-            <div className="contain">
-                <div className="navbar-header">
-                  <a className='navbar-brand'>icReact</a>
-                  <ul className="nav navbar-nav navbar-right">
-                      <li><a href="/">Home</a></li>
-                      <li><a href="/detil">Detil</a></li>
-                      <li><a href="/dataIsi">Data Isi</a></li>
-                  </ul>
-                </div>
-            </div>
+        <div className="contain">
+          <a className="navbar-brand navbar-left">icReact</a>
+            <ul className="nav navbar-nav navbar-right">
+              <li><a href="/">Home</a></li>
+              <li><a href="/detil">Detil</a></li>
+              <li><a href="/dataIsi">Data Isi</a></li>
+            </ul>
+        </div>
       </div>
     );
   }
